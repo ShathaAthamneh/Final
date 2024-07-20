94ef171d8946cdad5a8974dc3662d2205f5064b6
ShathaAthamneh <sathamneh5@gmail.com>
2024-07-20 20:53:54 +0300

    Add index.html and script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Prompt the user for their name
+let userName = prompt("What's your name?");
+
+// Send a welcoming message
+if (userName) {
+    alert("Welcome, " + userName + "!");
+} else {
+    alert("Welcome, Guest!");
+}
+
